35f6d626b03f132652ee12b4dfac66d0d4599837
SkilledSparrow <as850082@gmail.com>
2023-08-22 03:07:11 +0530

    completed

--- components/PromptInput.tsx
+++ components/PromptInput.tsx
@@ -1,7 +1,9 @@
 "use client";
+import fetchImages from "@/lib/FetchImages";
 import fetchSuggestionFromChatGPT from "../lib/fetchSuggestionFromChatGPT";
-import React, { useState } from "react";
+import React, { FormEvent, useState } from "react";
 import useSWR from "swr";
+import toast from "react-hot-toast";
 
 const PromptInput = () => {
   const [input, setInput] = useState("");
@@ -13,12 +15,53 @@ const PromptInput = () => {
   } = useSWR("/api/suggestion", fetchSuggestionFromChatGPT, {
     revalidateOnFocus: false,
   });
+  const { mutate: updateImages } = useSWR("images", fetchImages, {
+    revalidateOnFocus: false,
+  });
   // console.log(suggestion);
+  const submitPrompt = async (useSuggestion?: boolean) => {
+    const inputPrompt = input;
+    setInput("");
+    console.log(inputPrompt);
+    const notificationPrompt = inputPrompt || suggestion;
+    const notificationPromptShort = notificationPrompt.slice(0, 20);
+    const notification = toast.loading(
+      `DALL·E is creating: ${notificationPromptShort}...`
+    );
+
+    const p = useSuggestion ? suggestion : inputPrompt;
+
+    const res = await fetch("/api/generateImage", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ prompt: p }),
+    });
+    const data = await res.json();
+
+    if (data.error) {
+      toast.error(data.error);
+    } else {
+      toast.success(`Your AI Art has been Generated!`, {
+        id: notification,
+      });
+    }
+    updateImages();
+  };
+  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    await submitPrompt();
+  };
 
   const loading = isLoading || isValidating;
   return (
     <div className="m-10">
-      <form className="flex flex-col lg:flex-row shadow-md shadow-slate-400/10 border rounded-md lg:divide-x">
+      <form
+        onSubmit={handleSubmit}
+        className="flex flex-col lg:flex-row shadow-md shadow-slate-400/10 border rounded-md lg:divide-x"
+      >
         <textarea
           value={input}
           onChange={(e) => setInput(e.target.value)}
@@ -44,6 +87,7 @@ const PromptInput = () => {
           className="p-4 bg-violet-400 text-white transition-colors duration-200 font-bold disabled:text-gray-300
             disabled:cursor-not-allowed disabled:bg-gray-400"
           type="button"
+          onClick={() => submitPrompt(true)}
         >
           Use Suggestion
         </button>
